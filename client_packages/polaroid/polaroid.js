723681caa59e65cb8a61ed06fdf4633959f3d80a
TheMikeontheMic <52206136+TheMikeontheMic@users.noreply.github.com>
2021-08-10 14:18:05 -0600

    Bug Fixes.
    Fixed a bug where players could use the camera when falling or jumping and it would freeze them mid-air.

--- client_packages/polaroid/polaroid.js
+++ client_packages/polaroid/polaroid.js
@@ -19,6 +19,9 @@ let axisYModifer = 0;
 let axisZModifer = 1.0;
 let fovModifer = 60;
 let lastCamRot;
+let isRagdoll;
+let isJumping;
+
 
 //Mouse control varibles
 const mouseSensitivity = 2.5;
@@ -32,6 +35,11 @@ let selfieCamera = false;
 let selfieCamRotZ;
 let camRot;
 const player = mp.players.local;
+let phoneObject = mp.objects.new(413312110, player.position, {
+    dimension: 0
+});
+
+phoneObject.setVisible(false, false);
 
 
 
@@ -41,26 +49,25 @@ mp.events.add('render', () => {
 
     let zoomIn = (mp.game.controls.getDisabledControlNormal(2, 40) * zoomSpeed);
     let zoomOut = (mp.game.controls.getDisabledControlNormal(2, 41) * zoomSpeed);
-    if (PolaroidActive) {
+    if (PolaroidActive && !isRagdoll && !isJumping) {
         mp.game.controls.disableAllControlActions(2);
         mp.game.ui.displayRadar(false);
+
 
         camera.setActive(true);
         mp.game.cam.renderScriptCams(true, false, 0, true, false);
 
         if (player.vehicle && camera && !selfieCamera) {
             const playerPos = player.position;
-            camera.setCoord(playerPos.x, playerPos.y, playerPos.z + .7);
-        } else if (player.vehicle && camera && selfieCamera){
-            const playerPos = player.position;
-            camera.setCoord(playerPos.x - .5, playerPos.y + .5, playerPos.z + .7);
-            camera.setRot(playerPos.x, playerPos.y, playerPos.z + 180, 2);
+            camera.setCoord(playerPos.x, playerPos.y, playerPos.z + .6);
+        } else if (player.vehicle && camera && selfieCamera) {
+            mp.gui.chat.push(`Sorry you are unable to use selfie mode in a vehicle.`);
         }
 
         camRot = new mp.Vector3(camRot.x - y, 0, camRot.z - x);
         if (camRot.x <= (selfieCamera ? 15 : 30) && camRot.x >= (selfieCamera ? -15 : -10)) {
             camera.setRot(camRot.x, camRot.y, camRot.z, 2);
-            mp.gui.chat.push(`${camRot.x}`)
+            //mp.gui.chat.push(`${camRot.x}`)
         } else if (camRot.x < (selfieCamera ? 15 : 30)) {
             camRot.x = (selfieCamera ? -15 : -10);
         } else if (camRot.x > (selfieCamera ? -20 : -10)) {
@@ -99,6 +106,9 @@ mp.events.add('render', () => {
         if (!player.vehicle) {
             player.clearTasks();
             player.freezePosition(false);
+            setTimeout(() => {
+                phoneObject.setVisible(PolaroidActive, false);
+            }, 200);
         }
         selfieCamera = false;
     }
@@ -109,21 +119,32 @@ mp.keys.bind(o, true, function () {
     const graphics = mp.game.graphics;
     const playerPos = player.position;
 
+    isRagdoll = player.isRagdoll();
+    isJumping = player.isJumping();
+
+    // mp.gui.chat.push(`DEBUG: RagDoll state = ${isRagdoll} and Jumping State = ${isJumping}`);
+
+
     if (!mp.game.graphics.hasScaleformMovieLoaded(_handle) && !taskCompleted) {
         graphics.pushScaleformMovieFunction(_handle, 'OPEN_SHUTTER');
         graphics.pushScaleformMovieFunctionParameterInt(2);
-
         graphics.popScaleformMovieFunctionVoid();
         taskCompleted = true;
     }
 
     camRot = lastCamRot = mp.game.cam.getGameplayCamRot(2);
-    mp.gui.chat.push(`${JSON.stringify(mp.game.cam.getGameplayCamRot(2))}`)
+    //mp.gui.chat.push(`${JSON.stringify(mp.game.cam.getGameplayCamRot(2))}`)
     camera = mp.cameras.new('peekThrough', new mp.Vector3(playerPos.x + axisXModifer, playerPos.y + axisYModifer, playerPos.z + axisZModifer), camRot, fovModifer);
     PolaroidActive = !PolaroidActive;
     mp.gui.chat.show(false);
     mp.gui.chat.activate(false);
-    if (!player.vehicle) {
+    if (!player.vehicle && !isRagdoll && !isJumping) {
+
+        setTimeout(() => {
+            phoneObject.attachTo(player.handle, 90, 0, 0, 0, 0, 0, lastCamRot.z, true, true, false, false, 0, false);
+            phoneObject.setVisible(PolaroidActive, true);
+        }, 200);
+
         player.freezePosition(true);
         PolaroidActive ? mp.events.callRemote('startAnimation', 'cellphone@self@michael@', 'finger_point') : mp.events.callRemote('stopAnimation');
     }
@@ -132,10 +153,10 @@ mp.keys.bind(o, true, function () {
 mp.keys.bind(tab, true, function () {
     const playerPos = player.position;
     selfieCamera = !selfieCamera;
-    if (selfieCamera) {
+    if (selfieCamera && !player.vehicle) {
         mp.events.callRemote('updatePlayerRotation2');
     } else {
-        mp.gui.chat.push(`${JSON.stringify(lastCamRot)}`)
+        //mp.gui.chat.push(`${JSON.stringify(lastCamRot)}`)
         camRot = lastCamRot;
         camera = mp.cameras.new('peekThrough', new mp.Vector3(playerPos.x + axisXModifer, playerPos.y + axisYModifer, playerPos.z + axisZModifer), lastCamRot, fovModifer);
         selfieCamera = !selfieCamera;
@@ -145,7 +166,7 @@ mp.keys.bind(tab, true, function () {
 mp.events.addDataHandler('heading', function (entity, value) {
     if (selfieCamera && PolaroidActive) {
         const playerPos = player.position;
-        mp.gui.chat.push(`${value}`);
+        //mp.gui.chat.push(`${value}`);
         camRot = new mp.Vector3(0, 50, value - 180);
         player.setRotation(0, 0, value + 28, 0, true);
         camera = mp.cameras.new('selfieCamera', new mp.Vector3(playerPos.x + (0.8 * Math.sin(-value * Math.PI / 180)), playerPos.y + (0.8 * Math.cos(-value * Math.PI / 180)), playerPos.z + 0.6), camRot, fovModifer);
